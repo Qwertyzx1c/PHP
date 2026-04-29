d0da96c1eee14ed2948a09f3d6e5211536114d67
Qwertyzx1c <jakub.ura10@gmail.com>
2026-04-29 13:13:13 +0200

    Create zadania.php

---
+++ zadania.php
@@ -0,0 +1,42 @@
+1.
+<?php
+
+$imie = "Jakub";
+
+echo "Mam na imie $imie";
+
+?>
+
+2.
+<?php
+
+$a = 5;
+$b = 10;
+
+echo $a + $b;
+
+?>
+
+3.
+
+4.
+<?php
+
+$a = 5;
+$b = 10;
+
+echo $a * $b;
+
+?>
+
+5.
+<?php
+
+$a = 5;
+$b = 10;
+
+echo $a + $b / 2;
+
+?>
+
+6.
