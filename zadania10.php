61b23fea80622fec3eef5edc5807887aa4bd5bfa
Qwertyzx1c <jakub.ura10@gmail.com>
2026-09-24 13:49:52 +0200

    Create zadania10.php

---
+++ zadania10.php
@@ -0,0 +1,57 @@
+<?php
+class Person {
+    public $name;
+    public $age;
+
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+        echo "Tworzę człowieka $name, i lat $age\n";
+        echo "<br>";
+    }
+    
+
+    public function greet($name, $age) {
+        echo "<br>";
+        return "Cześć $name, lat $age";
+        
+    }
+    public function setAge($age) {
+        echo "<br>";
+        return "$age";
+        
+    }
+    public function isAdult($age) {
+        echo "<br>";
+        if ($age >= 18){
+            return "True";
+        }
+        elseif ($age < 18){
+            return "False";
+        }
+    }
+    public static function square($x){
+        echo "<br>";
+        return $x * $x;
+    }
+}
+
+$person = new Person("Paweł", 213777777777);
+echo $person->name;
+echo $person->greet("Jamnik", 1234567890);
+echo $person->setAge(mt_rand(1, 100));
+echo $person->isAdult(mt_rand(1, 100));
+echo Person::square(mt_rand(1, 100));
+echo "<br>";
+echo "<br>";
+
+class Circle {
+    public function area($a , $b){
+        return $a * $b ** 2;
+        echo "<br>";
+    }
+
+}
+
+$calc = new Circle();
+echo $calc->area(3.14, 7);
